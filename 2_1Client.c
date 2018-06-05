e48ed3ff7b5d64108dc2bffcfe742461f518c818
finewang1998 <39902850+finewang1998@users.noreply.github.com>
2018-06-05 06:46:08 +0000

    Update 2_1Client.c

--- 2_1Client.c
+++ 2_1Client.c
@@ -13,8 +13,10 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    char hello[20];
     char buffer[1024] = {0};
+    printf("please input the data:");
+    scanf("%s",&hello);
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
